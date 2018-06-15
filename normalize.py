dda42a2aaa00ceb6a3395c11796d2430d3f7be51
pmouser1 <574833620@qq.com>
2018-06-15 17:23:47 +0800

    0615

--- normalize.py
+++ normalize.py
@@ -6,3 +6,10 @@ def normalize(name):
 
 def produce(L):
 	return reduce(lambda x, y: x*y, L)
+
+def str2float(s):
+   def fn(x, y):
+        return x * 10 + y
+   def char2num(s):
+         return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}[s]
+   return reduce(fn, map(char2num, s.replace(".","")))
