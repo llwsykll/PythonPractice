8ab751854d910dd844b2bbddef82025d6aa7e838
llwsykll <574833620@qq.com>
2018-07-23 15:21:12 +0800

    下不来flask包！森气！不学了！都怪陈秋猪！

--- screen2.py
+++ screen2.py
@@ -1,32 +1,47 @@
-# -*- coding: utf-8 -*-
-class Screen(object):
-    
-    @property
-    def width(self):
-    	return self._width
-
-    @width.setter
-    def width(self,width):
-    	if not isinstance(width,int):
-    		raise ValueError('score must be an integer')
-    	if width < 0:
-    		raise ValueError('score must over zero')
-        self._width = width
-
-        
-    @property
-    def height(self):
-    	return self._height
-
-    @height.setter
-    def height(self,height):
-    	if not isinstance(height,int):
-            raise ValueError('score must be an integer')
-        if height < 0:
-            raise ValueError('score must over zero')
-        else:
-            self._height = height
-
-    @property 
-    def resolution(self):
-        return self._width*self._height
+function getArticleTypeByDept(staffId,departmentId,articleType,oaBaseUrl,article) {
+	console.log(article);
+	var _optDeptRole = 0;
+  $(departmentId).on('change', function () {
+    var path = $(oaBaseUrl).val();
+    var deptId = $(departmentId).find('option:selected').val();
+    if (deptId != 5) {
+                if (staffId) {
+                $.ajax({
+                  url:path+'/department/role',
+                  type:'POST',
+                  dataType: 'json',
+                  contentType: 'application/json;charset=UTF-8',
+                  data:JSON.stringify({
+                       "departmentId": deptId,
+                        "staffId": $(staffId).val()	
+                  }),
+                  success: function (resDeptRole) {
+                      if(resDeptRole.data != "部门分管领导"){
+                        _optDeptRole = 1;
+						$(articleType).find('option[value="1140"]').remove();
+					}
+                    if ($(articleType).val()) {
+                      $(articleType).val('').trigger('change');
+                    }
+                  }
+                })
+              }
+    }
+	else{
+		var flag = false;
+		console.log($(articleType).find('option[value="1140"]'));
+		var options = $(articleType).find('option');
+		console.log(options.length);
+		console.log(options);
+		for(var i=0;i<options.length;i++){
+			console.log('option',options[i]);
+			if(options[i].value==1140){
+			flag=true;}
+		}
+		if(options.length == 3){
+			var html='<option value="1140">监管部门文件1</option>';
+			$(articleType).append(html);
+		}
+	}
+  });
+}
